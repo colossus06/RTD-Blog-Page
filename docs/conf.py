9477813a6115d6a15821da52f08ba04c3d473280
colossus06 <denizkavuk86@gmail.com>
2022-08-17 17:53:40 -0400

    rtd-theme change

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
 
 
 
@@ -64,7 +64,7 @@ html_context = {
   "commit": False,
 }
 
-
+html_show_sourcelink = False
 
 
 def setup(app):
