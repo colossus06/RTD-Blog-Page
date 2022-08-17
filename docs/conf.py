14ed3e2a7f80de1565714b120de05f68541a4767
colossus06 <denizkavuk86@gmail.com>
2022-08-17 05:34:49 -0400

    removing version

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,11 @@ html_logo = "logo.png"
 
 
 
-
+html_context = {
+  "display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+  "last_updated": True,
+  "commit": False,
+}
 
 
 
